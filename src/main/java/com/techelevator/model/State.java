e47bb0b41aac01d703b0ec7c31fd148d8158e3c1
ttomlin <treytomlin91@gmail.com>
2018-08-20 14:21:24 -0400

    trey

--- src/main/java/com/techelevator/model/State.java
+++ src/main/java/com/techelevator/model/State.java
@@ -2,16 +2,33 @@ package com.techelevator.model;
 
 public class State {
 
+private int id;
 
-		private String state;
+private String name;
 
-		public String getState() {
-			return state;
-		}
+private String code;
 
-		public void setState(String state) {
-			this.state = state;
-		}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getCode() {
+	return code;
+}
+public void setCode(String code) {
+	this.code = code;
+}
+
+
+
 		
 		
 }
