13c839b289553e895c0d16f058f51ed3ebd33843
Timothy <timothyhart6@gmail.com>
2018-08-20 16:22:09 -0400

    connecting api and url

--- src/main/webapp/js/senators.js
+++ src/main/webapp/js/senators.js
@@ -1,11 +1,29 @@
 var allTheDatas;
 $(document).ready(function () {
 
+	function getQueryVariable(variable)
+	{
+	       var query = window.location.search.substring(1);
+	       var vars = query.split("&");
+	       for (var i=0;i<vars.length;i++) {
+	               var pair = vars[i].split("=");
+	               if(pair[0] == variable){return pair[1];}
+	       }
+	       return(false);
+	}
 	
     $("#btnSearch").on("click", function (event) {
-    	var parts = "http://localhost:8080/capstone/state?CSRF_TOKEN=jmeKjBtjYShPb4tJohbQUg%3D%3D&locationName=Alabama".split('=');
-    	var userAddress = parts.pop() || parts.pop();
+    	//var parts = "http://localhost:8080/capstone/state?CSRF_TOKEN=jmeKjBtjYShPb4tJohbQUg%3D%3D&locationName=Alabama".split('=');
+    	//var userAddress = parts.pop() || parts.pop();
+    	
+    	
+    	var userAddress = getQueryVariable("locationName");
     	alert(userAddress);
+      
+    	
+    	
+    	
+    	
         var url = "https://www.googleapis.com/civicinfo/v2/representatives";
         
         
